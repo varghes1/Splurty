eff40c7d0b102c98facf67012e22b79d35af2b28
Bijoy Varghese <bijoy.varghese.1990@gmail.com>
2015-12-16 01:35:07 +0000

    tdd model test

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -19,4 +19,5 @@ class QuotesController < ApplicationController
 	def quote_params
 		params.require(:quote).permit(:saying, :author)
 	end
+
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "unique_tag" do
+		quote = Quote.create(:author => 'Bijoy Varghese', :saying => 'What a day!')
+		expected = 'BV#' + quote.id.to_s
+		actual = quote.unique_tag
+		assert_equal expected, actual
+	end
+
+
 end
